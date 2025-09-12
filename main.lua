c22be71950f7846312bc74d2d75126e9e044021a
excavator48 <xdimskiy@gmail.com>
2025-09-12 17:08:07 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+--The Seventh Sea
 SMODS.Atlas{
     key = 'TheSeventhSea',
     path = 'TheSeventhSea.png',
@@ -32,7 +33,6 @@ SMODS.Joker{
       }
     },
     loc_vars = function(self,info_queue,center)
-        info_queue[#info_queue+1] = G.P_CENTERS.j_joker --adds "Joker"'s description next to this card's description
         return {vars = {center.ability.extra.Xmult}} --#1# is replaced with card.ability.extra.Xmult
     end,
     check_for_unlock = function(self, args)
@@ -54,4 +54,51 @@ SMODS.Joker{
             end                    
         end
     end    
+}
+
+
+--Crooked Penny
+SMODS.Atlas{
+    key = 'Crooked Penny',
+    path = 'Crooked Penny.png',
+    px = 71,
+    py = 95
+}
+
+SMODS.Joker{
+       key = 'Crooked Penny', --joker key
+    loc_txt = { -- local text
+        name = 'Crooked Penny',
+        text = {
+          'Reduces all {C:attention}listed{}',
+          '{C:green}probabilities{}',
+          '{C:inactive}(ex:{} {C:green}1 in 3{} {C:inactive}->{} {C:green}0.5 in 3{}{C:inactive}){}'
+        },
+        --[[unlock = {
+            'Be {C:legendary}cool{}',
+        }]]
+    },
+    atlas = 'Crooked Penny', --atlas' key
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 3, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = false, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 0, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+        loc_vars = function(self, info_queue, card)
+        return { vars = { number_format(10000) } }
+    end,
+    check_for_unlock = function(self, args)                      -- equivalent to `unlock_condition = { type = 'chip_score', chips = 10000 }`
+        return args.type == 'chip_score' and args.chips >= 10000 -- See note about Talisman on the wiki
+    end,
+        calculate = function(self, card, context)
+        if context.mod_probability and not context.blueprint then
+            return {
+                numerator = context.numerator * 0.5
+            }
+        end
+    end
 }
